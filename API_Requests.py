62aedfe94469b0918f8c6b2f3df81bc0a719d774
Ileriodef <47086473+IlerioDef@users.noreply.github.com>
2022-09-16 21:33:47 -0400

    upd

--- API_Requests.py
+++ API_Requests.py
@@ -1,11 +1,13 @@
 import requests
 
-test_url = "http://www.cbr.ru/scripts/XML_dynamic.asp?date_req1=02/03/2001&date_req2=14/03/2001&VAL_NM_RQ=R01235"
+test_url = "http://www.cbr.ru/scripts/XML_dynamic.asp"
+payload = {"date_req1:": "01/01/2022", "date_req2": "01/01/2022", "VAL_NM_RQ": "R01235"}
 """date_req1 = period starting date
 date_trq2 = period ending date
 VAL_NM_RQ = unique currency code used by Central Bank
 """
-cbr = requests.get(test_url)
-print(cbr)
 
+cbr = requests.get(test_url, params=payload)
+var = cbr.json
 ##### https://cbr.ru/scripts/XML_val.asp?d=0 - currency codes
+print(var)
