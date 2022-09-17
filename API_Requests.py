bd0693447212e3ae799c972b82daf9e98473017d
Ileriodef <47086473+IlerioDef@users.noreply.github.com>
2022-09-16 20:51:18 -0400

    addendum

--- API_Requests.py
+++ API_Requests.py
@@ -1,5 +1,9 @@
 import requests
 
 test_url = "http://www.cbr.ru/scripts/XML_dynamic.asp?date_req1=02/03/2001&date_req2=14/03/2001&VAL_NM_RQ=R01235"
+"""date_req1 = period starting date
+date_trq2 = period ending date
+VAL_NM_RQ = unique currency code used by Central Bank
+"""
 cbr = requests.get(test_url)
 print(cbr)
